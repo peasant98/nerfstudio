cd803d18e12a03a1ea71362424b20c0c4423400f
Matt Strong <mastro1@stanford.edu>
2024-02-07 11:39:42 -0800

    depth supervision GS fully working!

--- nerfstudio/configs/method_configs.py
+++ nerfstudio/configs/method_configs.py
@@ -73,9 +73,11 @@ from nerfstudio.models.tensorf import TensoRFModelConfig
 from nerfstudio.models.vanilla_nerf import NeRFModel, VanillaModelConfig
 from nerfstudio.models.ds_vanilla_nerf import DSNeRFModel, DSVanillaModelConfig
 from nerfstudio.pipelines.base_pipeline import VanillaPipelineConfig
-from nerfstudio.data.datamanagers.full_images_datamanager import FullImageDatamanagerConfig
+from nerfstudio.data.datamanagers.full_images_datamanager import FullImageDatamanagerConfig, FullImageDatamanager
 from nerfstudio.pipelines.dynamic_batch import DynamicBatchPipelineConfig
 from nerfstudio.plugins.registry import discover_methods
+
+from nerfstudio.models.depth_gaussian_splatting import DepthGaussianSplattingModelConfig
 
 method_configs: Dict[str, TrainerConfig] = {}
 descriptions = {
@@ -259,6 +261,7 @@ method_configs["depth-nerfacto"] = TrainerConfig(
 )
 
 method_configs["instant-ngp"] = TrainerConfig(
+    
     method_name="instant-ngp",
     steps_per_eval_batch=500,
     steps_per_save=2000,
@@ -642,6 +645,8 @@ method_configs["neus-facto"] = TrainerConfig(
     vis="viewer",
 )
 
+
+
 method_configs["gaussian-splatting"] = TrainerConfig(
     method_name="gaussian-splatting",
     steps_per_eval_image=100,
@@ -656,6 +661,57 @@ method_configs["gaussian-splatting"] = TrainerConfig(
             dataparser=ColmapDataParserConfig(load_3D_points=True),
         ),
         model=GaussianSplattingModelConfig(),
+    ),
+    optimizers={
+        "xyz": {
+            "optimizer": AdamOptimizerConfig(lr=1.6e-4, eps=1e-15),
+            "scheduler": ExponentialDecaySchedulerConfig(
+                lr_final=1.6e-6,
+                max_steps=30000,
+            ),
+        },
+        "features_dc": {
+            "optimizer": AdamOptimizerConfig(lr=0.0025, eps=1e-15),
+            "scheduler": None,
+        },
+        "features_rest": {
+            "optimizer": AdamOptimizerConfig(lr=0.0025 / 20, eps=1e-15),
+            "scheduler": None,
+        },
+        "opacity": {
+            "optimizer": AdamOptimizerConfig(lr=0.05, eps=1e-15),
+            "scheduler": None,
+        },
+        "scaling": {
+            "optimizer": AdamOptimizerConfig(lr=0.005, eps=1e-15),
+            "scheduler": None,
+        },
+        "rotation": {"optimizer": AdamOptimizerConfig(lr=0.001, eps=1e-15), "scheduler": None},
+        "camera_opt": {
+            "optimizer": AdamOptimizerConfig(lr=1e-3, eps=1e-15),
+            "scheduler": ExponentialDecaySchedulerConfig(lr_final=5e-5, max_steps=30000),
+        },
+    },
+    viewer=ViewerConfig(num_rays_per_chunk=1 << 15),
+    vis="viewer",
+)
+
+
+method_configs["depth-gaussian-splatting"] = TrainerConfig(
+    method_name="depth-gaussian-splatting",
+    steps_per_eval_image=100,
+    steps_per_eval_batch=0,
+    steps_per_save=2000,
+    steps_per_eval_all_images=1000,
+    max_num_iterations=30000,
+    mixed_precision=False,
+    gradient_accumulation_steps={"camera_opt": 100},
+    pipeline=VanillaPipelineConfig(
+        datamanager=FullImageDatamanagerConfig(
+            _target=FullImageDatamanager[DepthDataset],
+            dataparser=ColmapDataParserConfig(load_3D_points=True),
+        ),
+        model=DepthGaussianSplattingModelConfig(),
     ),
     optimizers={
         "xyz": {
